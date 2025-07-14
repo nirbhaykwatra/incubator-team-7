bc9356ff4c00ef022bf73381d202b928b594cd1c
LelouKieh <qx@zju.edu.cn>
2025-07-14 11:55:38 -0700

    E to pick up nuclear battery

--- Assets/Internment/Scripts/Character/CharacterInteraction.cs
+++ Assets/Internment/Scripts/Character/CharacterInteraction.cs
@@ -3,52 +3,162 @@ using GameEvents;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class CharacterInteraction : MonoBehaviour
 {
+    [Header("Interaction Settings")]
     [SerializeField] private Camera _fpsCamera;
+    [SerializeField] private float InteractionRange = 5f;
+    [SerializeField] private LayerMask interactionMask = -1; // What layers can be interacted with
+
+    [Header("UI Feedback")]
+    [SerializeField] private GameObject interactionPrompt; // UI element showing "Press E to interact"
+    [SerializeField] private Text interactionText; // Text component for item name
+    [SerializeField] private string interactionKey = "E";
+
+    [Header("Events")]
     [SerializeField] private GameObjectEventAsset OnInteractableHover;
     [SerializeField] private GameObjectEventAsset OnInteractableClick;
-    [SerializeField] private float InteractionRange = 5f;
-    
-    private Vector3 _liftingPosition;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Debug")]
+    [SerializeField] private bool showDebugRay = true;
+
+    private IInteractable currentInteractable;
+    private GameObject currentInteractableObject;
+    private PickableObject currentPickable;
+
     private void Awake()
     {
-        _liftingPosition = Vector3.Cross(transform.forward, Vector3.forward);
+        if (_fpsCamera == null)
+            _fpsCamera = Camera.main;
+
+        // Hide interaction prompt initially
+        if (interactionPrompt != null)
+            interactionPrompt.SetActive(false);
     }
 
-    private void FixedUpdate()
+    private void Update()
+    {
+        CheckForInteractable();
+    }
+
+    private void CheckForInteractable()
     {
         RaycastHit hit;
-        
-        if (Physics.Raycast(_fpsCamera.gameObject.transform.position, _fpsCamera.gameObject.transform.forward, out hit, InteractionRange))
+        Ray ray = new Ray(_fpsCamera.transform.position, _fpsCamera.transform.forward);
+
+        if (Physics.Raycast(ray, out hit, InteractionRange, interactionMask))
         {
-            if (hit.collider.gameObject.TryGetComponent(out IInteractable interactable))
+            // Check if we hit an interactable
+            IInteractable interactable = hit.collider.GetComponent<IInteractable>();
+
+            if (interactable != null)
             {
-                OnInteractableHover?.Invoke(gameObject);
+                // New interactable found
+                if (interactable != currentInteractable)
+                {
+
+                    currentInteractable = interactable;
+                    currentInteractableObject = hit.collider.gameObject;
+                    currentPickable = hit.collider.GetComponent<PickableObject>();
+
+                    // Set hover state on new object
+                    if (currentPickable != null)
+                    {
+                        ShowInteractionPrompt(currentPickable.ItemName);
+                    }
+                    else
+                    {
+                        ShowInteractionPrompt("Interact");
+                    }
+
+                    OnInteractableHover?.Invoke(currentInteractableObject);
+                }
             }
+            else
+            {
+                ClearCurrentInteractable();
+            }
+        }
+        else
+        {
+            ClearCurrentInteractable();
+        }
+    }
+
+    private void ClearCurrentInteractable()
+    {
+        if (currentInteractable != null)
+        {
+
+            currentInteractable = null;
+            currentInteractableObject = null;
+            currentPickable = null;
+            HideInteractionPrompt();
         }
     }
 
     public void OnInteract(InputValue value)
     {
-        RaycastHit hit;
-        
-        if (Physics.Raycast(_fpsCamera.gameObject.transform.position, _fpsCamera.gameObject.transform.forward, out hit, InteractionRange))
+        if (!value.isPressed) return;
+
+        if (currentInteractable != null)
         {
-            if (hit.collider.gameObject.TryGetComponent(out IInteractable interactable))
+            // Check if it's already collected (for pickable objects)
+            if (currentPickable != null && currentPickable.IsPickedUp)
             {
-                OnInteractableClick?.Invoke(gameObject);
-                interactable.Interact();
+                Debug.Log("Item already collected!");
+                return;
+            }
+
+            // Perform the interaction
+            currentInteractable.Interact();
+            OnInteractableClick?.Invoke(currentInteractableObject);
+
+            // Clear the current interactable if it was picked up
+            if (currentPickable != null && currentPickable.IsPickedUp)
+            {
+                ClearCurrentInteractable();
             }
         }
     }
 
-    public void OnDrawGizmosSelected()
+    private void ShowInteractionPrompt(string itemName)
     {
-        Gizmos.color = Color.red;
-        if (_fpsCamera != null) Gizmos.DrawRay(_fpsCamera.gameObject.transform.position, _fpsCamera.gameObject.transform.forward * InteractionRange);
-        Gizmos.DrawSphere(_liftingPosition, 0.1f);
+        if (interactionPrompt != null)
+        {
+            interactionPrompt.SetActive(true);
+            if (interactionText != null)
+            {
+                interactionText.text = $"Press {interactionKey} to pick up {itemName}";
+            }
+        }
+    }
+
+    private void HideInteractionPrompt()
+    {
+        if (interactionPrompt != null)
+        {
+            interactionPrompt.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!showDebugRay) return;
+
+        Gizmos.color = currentInteractable != null ? Color.green : Color.red;
+        if (_fpsCamera != null)
+        {
+            Gizmos.DrawRay(_fpsCamera.transform.position, _fpsCamera.transform.forward * InteractionRange);
+
+            // Draw a sphere at the interaction point
+            if (currentInteractableObject != null)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(currentInteractableObject.transform.position, 0.5f);
+            }
+        }
     }
 }
--- Assets/Internment/Scripts/Utilities/PickableObject.cs
+++ Assets/Internment/Scripts/Utilities/PickableObject.cs
@@ -1,18 +1,67 @@
 using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PickableObject : MonoBehaviour, IInteractable
 {
+    [Header("Pickup Settings")]
+    [SerializeField] private string itemName = "Item";
+    [SerializeField] private bool destroyOnPickup = true;
+
+    [Header("Visual Feedback")]
+    [SerializeField] private GameObject pickupEffect;
+    [SerializeField] private AudioClip pickupSound;
+
     [ShowInInspector]
     [ReadOnly]
     private bool _isPickedUp;
     private GameObject _player;
+
+    [Header("Events")]
+    public UnityEvent<GameObject> OnPickedUp;
+    public UnityEvent<string> OnItemCollected;
+
+    private Collider _collider;
+    private Rigidbody _rigidbody;
+    private MeshRenderer _meshRenderer;
+
+    public string ItemName => itemName;
+    public bool IsPickedUp => _isPickedUp;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+
+        // Ensure the object has a collider
+        if (_collider == null)
+        {
+            _collider = gameObject.AddComponent<BoxCollider>();
+        }
+
+        _collider.isTrigger = false;
+    }
+
     public void Interact()
     {
-        Debug.Log($"isPickedUp: {_isPickedUp}");
-        _isPickedUp = !_isPickedUp;
+        if (_isPickedUp)
+        {
+            return;
+        }
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            Debug.LogWarning("No player found with 'Player' tag!");
+            return;
+        }
+
+        PickUp(player);
     }
+
+
     
     private void FixedUpdate()
     {
@@ -29,6 +78,28 @@ public class PickableObject : MonoBehaviour, IInteractable
 
     public void PickUp(GameObject player)
     {
-        _player = _isPickedUp ? null : player;
+        if (_isPickedUp)
+        {
+            return;
+        }
+
+        _isPickedUp = true;
+        Debug.Log($"Player picked up: {itemName}");
+
+        OnPickedUp?.Invoke(player);
+        OnItemCollected?.Invoke(itemName);
+
+        // Handle the object after pickup
+        if (destroyOnPickup)
+        {
+            if (_rigidbody != null) _rigidbody.isKinematic = true;
+            if (_collider != null) _collider.enabled = false;
+
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
